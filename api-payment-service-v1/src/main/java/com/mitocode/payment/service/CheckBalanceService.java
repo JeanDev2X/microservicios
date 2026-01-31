a9ae4ff570465787bdc70a8ea53723b112a95757
Jean Huaringa <jean@mit.com>
2026-01-31 09:19:41 -0500

    feature(api-payment-service-v1): Implementacion del api payment y conexion a clientes HTTP con RestTemplate

--- api-payment-service-v1/src/main/java/com/mitocode/payment/service/CheckBalanceService.java
+++ api-payment-service-v1/src/main/java/com/mitocode/payment/service/CheckBalanceService.java
@@ -1,6 +1,7 @@
 package com.mitocode.payment.service;
 
 import com.mitocode.payment.client.VisaRestTemplateClient;
+import com.mitocode.payment.client.dto.CheckBalanceRequest;
 import com.mitocode.payment.client.dto.CheckBalanceResponse;
 import com.mitocode.payment.domain.CheckBalance;
 import com.mitocode.payment.repository.CheckBalanceRepository;
@@ -18,10 +19,15 @@ public class CheckBalanceService {
 
     public boolean checkFunds(CheckBalance checkBalance) {
 
+        //validar el balance a traves del cliente rest template
         CheckBalanceResponse response = visaClient.checkBalance(checkBalance.getCardId(), checkBalance.getRequiredAmount());
+
+        //comunicacion con visa
+        //CheckBalanceResponse response = visaClient.checkBalance(checkBalance.getCardId(), checkBalance.getRequiredAmount());
 
         checkBalance.setId(UUID.randomUUID());
 
+        //Resitra el resultado del checkeo, de las consultas a visa
         return repository.saveCheckResult(checkBalance, response.sufficient());
     }
 }
