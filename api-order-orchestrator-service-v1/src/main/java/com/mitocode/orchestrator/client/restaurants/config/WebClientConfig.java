29600ace5de6e4b42ddb2cf3f57af9e725da0f9d
Jean Huaringa <jean@mit.com>
2026-02-18 17:26:09 -0500

    properties(applications-dev): se actualiza las URL de los clientes http internos para que apunten al nombre del servicio en eureka en lugar de a un IP:PUERTO

--- api-order-orchestrator-service-v1/src/main/java/com/mitocode/orchestrator/client/restaurants/config/WebClientConfig.java
+++ api-order-orchestrator-service-v1/src/main/java/com/mitocode/orchestrator/client/restaurants/config/WebClientConfig.java
@@ -1,6 +1,7 @@
 package com.mitocode.orchestrator.client.restaurants.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,15 +10,17 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
+    @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
 
     @Bean
     public WebClient restaurantWebClient(
-            @Value("${http-clients.internal.api-restaurant-service-v1.base-url}")
-            String baseUrl,
+            //@Value("${http-clients.internal.api-restaurant-service-v1.base-url}")
+            //String baseUrl,
             WebClient.Builder webClientBuilder) {
+        String baseUrl = "http://api-restaurant-service-v1";
         return webClientBuilder
                 .clone()
                 .baseUrl(baseUrl)
