71686b0dc8944ca10ddd14fa7caf8b51d3b62c9d
Jean Huaringa <jean@mit.com>
2026-02-25 10:32:49 -0500

    fix(order-orchestrator): fix para usar RestClient con Eureka

--- api-order-orchestrator-service-v1/src/main/java/com/mitocode/orchestrator/client/ordes/config/OrderRestClientBaseConfig.java
+++ api-order-orchestrator-service-v1/src/main/java/com/mitocode/orchestrator/client/ordes/config/OrderRestClientBaseConfig.java
@@ -9,12 +9,18 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class OrderRestClientBaseConfig {
 
-    @Bean("loadBalancedRestClientBuilder")
+    //usamos @LoadBalanced para indicar que este RestClient.Builder debe ser configurado para usar load balancing,
+    // lo que permite resolver el nombre del servicio a través de un balanceador de carga (como Ribbon o Spring Cloud LoadBalancer)
+    // en lugar de usar una URL fija, esto es necesario porque en el bean orderRestClient se configura la baseUrl con el nombre del
+    // servicio "http://API-ORDER-SERVICE-V1" en lugar de una URL concreta
+    @Bean("loadBalancedRestClientBuilder")//Indicamo que use el cliente con loadBalanced para resuelva por los nombres de servicio de Eureka
     @LoadBalanced
     public RestClient.Builder restClientBuilder() {
         return RestClient.builder();
     }
 
+    //usamos @Primary para indicar que este es el bean principal a usar cuando se inyecte un RestClient.Builder sin especificar el nombre del bean,
+    // esto es necesario porque tenemos otro RestClient.Builder definido en esta clase con el nombre "loadBalancedRestClientBuilder"
     @Primary
     @Bean
     public RestClient.Builder cleanRestClientBuilder() {
