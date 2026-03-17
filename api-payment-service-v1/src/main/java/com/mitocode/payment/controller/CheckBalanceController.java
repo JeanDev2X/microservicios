3c46df6785355bcb790aa263ccf592e533054b56
Jean Huaringa <jean@mit.com>
2026-03-17 07:32:01 -0500

    api-gateway(case1): ejemplos de loadbalancer eureka y local, redireccionamiento, filtros por path, method y header y agregar headers a las peticiones

--- api-payment-service-v1/src/main/java/com/mitocode/payment/controller/CheckBalanceController.java
+++ api-payment-service-v1/src/main/java/com/mitocode/payment/controller/CheckBalanceController.java
@@ -3,6 +3,7 @@ package com.mitocode.payment.controller;
 import com.mitocode.payment.domain.CheckBalance;
 import com.mitocode.payment.dto.CheckBalanceRequest;
 import com.mitocode.payment.service.CheckBalanceService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,13 +23,15 @@ public class CheckBalanceController {
     }
 
     @PostMapping("/check-balance")
-    public ResponseEntity<Void> checkBalance(@RequestBody CheckBalanceRequest request) {
-        log.info("Checking balance");
+    public ResponseEntity<Void> checkBalance(@RequestBody CheckBalanceRequest request, HttpServletRequest servletRequest) {
+        String xDemoMode = servletRequest.getHeader("X-Demo-Mode");
+        String requestId = servletRequest.getHeader("X-Request-ID");
+        log.info("Checking balance with headers: X-Demo-Mode: {}, X-Request-ID: {}", xDemoMode, requestId);
+
         CheckBalance checkBalance = CheckBalance.createNew(request.customerId(), request.cardId(), request.requiredAmount());
         if (service.checkFunds(checkBalance)) {
             return ResponseEntity.ok().build();
         }
         return ResponseEntity.badRequest().build();
     }
-
 }
