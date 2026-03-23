0f995c7b876dabc7274d0463a8981538efa7e24c
Jean Huaringa <jean@mit.com>
2026-03-23 16:39:05 -0500

    saga(order-service): endpoints de compensacion + producer y consumer en api order query service v1

--- api-order-service-v1/src/main/java/com/mitocode/order/service/OrderService.java
+++ api-order-service-v1/src/main/java/com/mitocode/order/service/OrderService.java
@@ -3,12 +3,18 @@ package com.mitocode.order.service;
 import com.mitocode.order.domain.Order;
 import com.mitocode.order.domain.OrderStatus;
 import com.mitocode.order.dto.request.CancelOrderRequest;
+import com.mitocode.order.dto.request.UpdateOrderStatusRequest;
+import com.mitocode.order.dto.response.OrderResponse;
+import com.mitocode.order.mapper.DomainToResponseMapper;
+import com.mitocode.order.producer.order.cancelled.OrderCancelledProducer;
 import com.mitocode.order.producer.order.created.OrderCreatedProducer;
+import com.mitocode.order.producer.order.updated.OrderUpdatedProducer;
 import com.mitocode.order.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -17,6 +23,8 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderCreatedProducer orderCreatedProducer;
+    private final OrderUpdatedProducer orderUpdatedProducer;
+    private final OrderCancelledProducer orderCancelledProducer;
 
     @Transactional
     public Order create(Order domain) {
@@ -27,6 +35,24 @@ public class OrderService {
         orderCreatedProducer.produce(orderSaved);
 
         return orderSaved;
+    }
+
+    @Transactional
+    public OrderResponse updateOrderStatus(UUID orderId, UpdateOrderStatusRequest request) {
+        Optional<Order> opt = orderRepository.findById(orderId);
+        if (opt.isEmpty()) {
+            throw new IllegalArgumentException("Order not found: " + orderId);
+        }
+
+        Order domain = opt.get();
+
+        // update entity (or convert to domain and apply business method)
+        domain.setStatus(request.status());
+        Order updated = orderRepository.save(domain);
+
+        orderUpdatedProducer.produce(updated);
+        // return response
+        return DomainToResponseMapper.toResponse(updated);
     }
 
     public Order cancelOrder(UUID orderId, CancelOrderRequest request) {
@@ -40,6 +66,7 @@ public class OrderService {
         Order cancelledOrder = orderRepository.save(domain);
 
         //producer para actualizar el estado y la razon de cancelacion de la orden
+        orderCancelledProducer.produce(cancelledOrder);
 
         return cancelledOrder;
     }
